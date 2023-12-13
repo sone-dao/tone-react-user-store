5a26358679b2a4a4d16ef8c84d71c3c57b54bb55
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-13 11:07:19 -0500

    feat: user store skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-import ClientComponent from './ClientComponent'
-import ServerComponent from './ServerComponent'
+import { StoreApi, UseBoundStore, create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export { ClientComponent, ServerComponent }
+export type ToneUser = {
+  isLoggedIn: boolean
+  sessionId: string
+  userId: string
+  display: string
+  darkMode: boolean
+}
+
+export type UseUserStore = UseBoundStore<StoreApi<ToneUser>>
+
+export default create<ToneUser>()(
+  persist(
+    (set) => ({
+      isLoggedIn: false,
+      sessionId: '',
+      userId: '',
+      display: '',
+      darkMode: false,
+    }),
+    { name: 'tone.user' }
+  )
+)
